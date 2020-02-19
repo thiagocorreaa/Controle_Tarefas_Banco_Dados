7c2ccece290823b5099f43b2f9b97ce152b0c973
thiago.correa <thiago.correa@DBC-000561>
2020-02-19 20:23:16 -0300

    Add project files.

---
+++ Controle_Tarefas/dbo/Tables/Tarefas.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Tarefas] (
+    [Id_Tarefa]      INT           NOT NULL,
+    [Titulo]         VARCHAR (150) NOT NULL,
+    [Status]         BIT           NOT NULL,
+    [Descricao]      VARCHAR (500) NULL,
+    [Data_Criacao]   DATETIME      CONSTRAINT [DF_Tarefas_DataCriacao] DEFAULT (getdate()) NOT NULL,
+    [Data_Edicao]    DATETIME      NULL,
+    [Data_Remocao]   DATETIME      NULL,
+    [Data_Conclusao] DATETIME      NULL,
+    CONSTRAINT [PK_Tarefas] PRIMARY KEY CLUSTERED ([Id_Tarefa] ASC)
+);
+
